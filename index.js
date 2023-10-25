1c75d0b77a80d75c79b46dba12241b040f4c32ea
wydfaxil7 <a.faazil07@gmail.com>
2023-10-25 15:32:06 +0500

    added script js file with name

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ faqItems.forEach((faq) =>{
     const toggleButton = faq.querySelector('.faq-toggle');
     toggleButton.addEventListener('click', () =>{
         const faqText = faq.querySelector('.faq-text');
-        toggleButton.parentNode.classList.toggle('.active')
+        faqText.classList.toggle('.active');
     })
 })

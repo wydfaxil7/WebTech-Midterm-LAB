afa9333b4aac0470056c279438e35293a32e78db
wydfaxil7 <a.faazil07@gmail.com>
2023-10-25 15:30:00 +0500

    modifeid JS File

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const faqItems = document.querySelectorAll('.faq');
 
 faqItems.forEach((faq) =>{
-    
+    const toggleButton = faq.querySelector('.faq-toggle');
+    toggleButton.addEventListener('click', () =>{
+        const faqText = faq.querySelector('.faq-text');
+        toggleButton.parentNode.classList.toggle('.active')
+    })
 })
